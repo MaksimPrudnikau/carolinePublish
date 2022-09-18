b9a2858abcde2b0cf3772e2dc3cee7fc93d7b45c
maksimprudnikau <maksimprudnikau@gmail.com>
2022-09-18 18:44:52 +0300

    init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+    $("#row").click(function() {
+        const english = $("#english");
+        if (english.css('visibility') === 'hidden') {
+            english.css({'visibility': 'visible'});
+        } else
+        {
+            english.css({'visibility': 'hidden'});
+        }
+    });
+});
